bdec0f529997c50070ef7f8eeac9c63748026be5
msrajeshkannan10 <92937770+msrajeshkannan10@users.noreply.github.com>
2021-10-30 09:29:17 +0530

    Add files via upload

---
+++ interview 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define THIS 0x04
+#define THAT 0x03
+
+int main()
+
+{
+    
+unsigned char va=0x00;
+    
+va |= THIS;
+   
+ va |= THAT;
+    
+if(va == THIS & THAT)
+    
+printf("%d",THAT);
+    
+if(va & THAT << 2)
+    
+printf("%d",THIS);
+    
+return 0;
+
+}
---
+++ interview.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+
+{
+   
+ unsigned char c=(int)0.54;
+    
+for(;c++;printf("%d",c));
+    
+printf("%d",c);
+   
+ return 0;
+
+}
+
+
+
+#include <stdio.h>
+
+int main()
+
+{
+   
+ unsigned char c=(int)0.54;
+    
+for(;c++;printf("%f",c));
+    
+printf("%f",c);
+   
+ return 0;
+
+}
+
+
+#include <stdio.h>
+
+int main()
+
+{
+    
+unsigned char c=(int)0.54;
+    
+for(; ;)
+   
+ {
+    
+printf("%d",c);
+   
+ c++;
+   
+ }
+
+}
